f17d3a04c6222a1b8ff969b0923f792a753c2b90
Atikur Rahman <atikassam@outlook.com>
2024-04-22 21:51:25 +0530

    Initial commit

---
+++ mobile/src/screens/public/AuthorizationScreen.tsx
@@ -0,0 +1,7 @@
+import {Box, Text} from "@gluestack-ui/themed";
+
+export default function AuthorizationScreen() {
+    return <Box width="100%" justifyContent="center" alignItems="center" flex={1}>
+        <Text>ProfileEditScreen</Text>
+    </Box>
+}
